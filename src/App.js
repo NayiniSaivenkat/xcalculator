ae3137de6bcb7408c7e8306d16196035347e0d18
saivenkat nayini <nayinisaivenkat22@gmail.comgit config --global user.name NayiniSaivenkat>
2024-02-23 21:34:39 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [display, setDisplay] = useState('');
+    const [result,setResult] = useState('');
+
+    const appendToDisplay = value => {
+        setDisplay(prevDisplay => prevDisplay + value);
+    };
+
+    const clearDisplay = () => {
+        setResult('');
+        setDisplay("")
+    };
+
+    const calculate = () => {
+        let resultt;
+       
+            if (display.trim() === '') {
+                setResult("Error")
+            }
+            resultt = eval(display);
+            if (!isFinite(resultt)) {
+                setResult("Infinity")
+            }
+            if (isNaN(resultt)) {
+                setResult("NaN")
+            }
+            setResult(resultt.toString());
+        
+    };
+
+    return (
+        <div className="calculator">
+            <input type="text" value={display} readOnly />
+            <br />
+            <span>{result}</span>
+            <br/>
+            <button onClick={() => appendToDisplay('7')}>7</button>
+            <button onClick={() => appendToDisplay('8')}>8</button>
+            <button onClick={() => appendToDisplay('9')}>9</button>
+            <button onClick={() => appendToDisplay('+')}>+</button>
+            <br />
+            <button onClick={() => appendToDisplay('4')}>4</button>
+            <button onClick={() => appendToDisplay('5')}>5</button>
+            <button onClick={() => appendToDisplay('6')}>6</button>
+            <button onClick={() => appendToDisplay('-')}>-</button>
+            <br />
+            <button onClick={() => appendToDisplay('1')}>1</button>
+            <button onClick={() => appendToDisplay('2')}>2</button>
+            <button onClick={() => appendToDisplay('3')}>3</button>
+            <button onClick={() => appendToDisplay('*')}>*</button>
+            <br />
+            <button onClick={clearDisplay}>C</button>
+            <button onClick={() => appendToDisplay('0')}>0</button>
+            <button onClick={calculate}>=</button>
+            <button onClick={() => appendToDisplay('/')}>/</button>
+            <br />
+            
+        </div>
+    );
 }
 
 export default App;
+
